e9bba6287c4b3405886ac0e08beeb22df3694287
jarturogl05 <jarturogl05@gmail.com>
2021-06-09 11:37:43 -0500

    connection to db

---
+++ server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    
+    database: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "bank",
+        host: process.env.DB_HOST
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express();
+require('dotenv').config()
+const sequelize = require('./database/db');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+
+  sequelize.authenticate().then(() =>{
+    console.log('connection to database');
+  }).catch(error =>{
+    console.log(error)
+  })
 });
